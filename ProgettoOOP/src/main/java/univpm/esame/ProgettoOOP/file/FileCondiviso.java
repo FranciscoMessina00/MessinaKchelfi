3ab9fdca85878a9d9dd74695e7ff42d61335bf93
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-07 20:58:27 +0100

    modificacommento

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/file/FileCondiviso.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/file/FileCondiviso.java
@@ -3,12 +3,12 @@ package univpm.esame.ProgettoOOP.file;
 public class FileCondiviso extends File{
 	//Nel caso di file condiviso specifichiamo l'autore
 	private String autore;
-	//Costruttore della classe "File" senza parametri
+	//Costruttore della classe "File Condiviso" senza parametri
 	public FileCondiviso() {
 		super();
 		this.autore=null;
 	}
-	//Costruttore della classe "File" con i parametri
+	//Costruttore della classe "File Condiviso" con i parametri
 	public FileCondiviso(String nome, String estensione, String tipo, int grandezza , String link, boolean condivisione, String autore) {
 		super(nome, estensione, tipo, grandezza, link, condivisione);
 		this.autore=autore;
