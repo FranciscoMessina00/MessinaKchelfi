f986719ffee302b67359856461e8853b573b08ee
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-09 16:20:33 +0100

    controller

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/controller/Controller.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/controller/Controller.java
@@ -7,12 +7,6 @@ import univpm.esame.ProgettoOOP.model.*;
 
 @RestController
 public class Controller {
-<<<<<<< HEAD
-	public AbstractFile file= new PrivateFile("ciccia","txt","testo",500000L,"/cartella1");
-	@GetMapping("/files")
-	public String seeFile() {
-		return file.toString();
-=======
 	//public FileAstratto file= new FilePrivato("ciccia","txt","testo",500000L,"/cartella1");
 	@GetMapping("/files")
 	public String vediFile() {
@@ -21,7 +15,6 @@ public class Controller {
 	@GetMapping("/files/nome")
 	public String vediFileSpecifico() {
 		return ; //ritorna le caratteristiche del file
->>>>>>> f94c1877d9b94bdb51bd70e8c9c3865ccafac848
 	}
 	
 }
