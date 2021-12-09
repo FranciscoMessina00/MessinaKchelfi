eeba4469e01ab16c83ea5dc29e3302b932080dea
FranciscoMessina00 <s1092353@studenti.univpm.it>
2021-12-09 11:48:35 +0100

    Introduzione classe cartella

---
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/file/Cartella.java
@@ -0,0 +1,37 @@
+package univpm.esame.ProgettoOOP.file;
+
+public class Cartella {
+	private String nome;
+	private String path;
+	private String id;
+	public Cartella() {
+		this.nome = null;
+		this.path = null;
+		this.id = null;
+	}
+	public Cartella(String nome, String path, String id) {
+		this.nome = nome;
+		this.path = path;
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
