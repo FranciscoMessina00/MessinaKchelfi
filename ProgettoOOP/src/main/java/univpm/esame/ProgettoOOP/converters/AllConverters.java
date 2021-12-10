43cb028c1fe190a54d3fd5a85248534cc4894d6b
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-10 17:29:05 +0100

    corr

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/converters/AllConverters.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/converters/AllConverters.java
@@ -7,14 +7,14 @@ import org.json.simple.JSONObject;
 import univpm.esame.ProgettoOOP.model.AbstractObject;
 
 public interface AllConverters {
-	//with API list_folder we get in return a JSON object
-	//with API get_temporary_link we get in return a JSON object
+	//With API list_folder we get in return a JSON array
+	public JSONArray APIToJSONArray();
+	//With API get_temporary_link we get in return a JSON object
 	public JSONObject APIToJSONObject();
-	//this method transforms the JSONArray to a ArrayList<AbstractObject>
+	//This method transforms the JSONArray to a ArrayList<AbstractObject>
 	public ArrayList<AbstractObject> JSONArrayToObject();
-	//this method transforms a JSONObject to a AbstractObject;
+	//This method transforms a JSONObject to a AbstractObject;
 	public AbstractObject JSONToObject();
-	//this method transforms the JSONObject to a String that represents 
-	//the downloadable link of a File
+	//This method transforms the JSONObject to a String that represents the downloadable link of a File
 	public String JSONObjectToURLDownload();
 }
