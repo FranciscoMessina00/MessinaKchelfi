29eb988498395f1cf298b7dc16edcaf588607133
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-10 22:22:43 +0100

    filtriprova

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/ExtensionFilter.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/ExtensionFilter.java
@@ -1,22 +1,19 @@
 package univpm.esame.ProgettoOOP.util.filter;
-
-import univpm.esame.ProgettoOOP.model.*;
+import univpm.esame.ProgettoOOP.model.File;
 
 public class ExtensionFilter {
     private String input;
 
-//    public ExtensionFilter (Object input) throws **{
-//		if (input instanceof String) 
-//			this.input = (String) input;
-//		else throw new **("L'input deve essere una parola");
-//		}
-//
-//        public boolean doFiltro (AbstractFile){
-//            if (file.getExtension().equalsIgnoreCase(input))
-//                return true;
-//            else return false;
-//        }
-//
-//    }
-    
+    public ExtensionFilter (Object input){
+		if (input instanceof String) 
+			this.input = (String) input;
+		else throw new Exception("L'input deve essere una parola");
+		}
+
+       public boolean doFiltro (File file){
+           if (file.getExtension().equalsIgnoreCase(input))
+               return true;
+           else return false;       
+        }
+    }
 }
--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/NameFilter.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/NameFilter.java
@@ -1,21 +1,21 @@
 package univpm.esame.ProgettoOOP.util.filter;
 
-import univpm.esame.ProgettoOOP.model.*;
+import univpm.esame.ProgettoOOP.model.File;
 
 public class NameFilter {
     private String input;
 
-//    public NameFilter (Object input) throws **{
-//		if (input instanceof String) 
-//			this.input = (String) input;
-//		else throw new **("L'input deve essere una parola");
-//		}
-//
-//        public boolean doFilter (AbstractFile){
-//            if (file.getPath().toLowerCase().contains(input.toLowerCase()))
-//                return true;
-//            else return false;
-//        }
+    public NameFilter (Object input) throws **{
+		if (input instanceof String) 
+			this.input = (String) input;
+		else throw new **("L'input deve essere una parola");
+		}
+
+        public boolean doFilter (AbstractFile){
+            if (file.getPath().toLowerCase().contains(input.toLowerCase()))
+                return true;
+            else return false;
+        }
 
     
     
