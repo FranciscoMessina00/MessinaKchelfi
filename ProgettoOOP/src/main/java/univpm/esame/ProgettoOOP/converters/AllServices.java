3190d37c743ded9a19175e1b65ca3e7aa7971c4d
FranciscoMessina00 <s1092353@studenti.univpm.it>
2021-12-10 13:21:15 +0100

    Interfaccia e implementazione (ancora da scriverci il codice)

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/converters/AllServices.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/converters/AllServices.java
@@ -1,4 +1,6 @@
 package univpm.esame.ProgettoOOP.converters;
+import java.util.ArrayList;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -6,11 +8,14 @@ import univpm.esame.ProgettoOOP.model.AbstractObject;
 
 public interface AllServices {
 	//with API list_folder we get in return a JSON array
-	public JSONArray URLToJSONArray();
+	public JSONArray APIToJSONArray();
 	//with API get_temporary_link we get in return a JSON object
-	public JSONObject URLToJSONObject();
-	//this method transforms the JSON array to either File or Folder
-	public JSONObject JSONArrayToObject();
+	public JSONObject APIToJSONObject();
+	//this method transforms the JSONArray to a ArrayList<AbstractObject>
+	public ArrayList<AbstractObject> JSONArrayToObject();
+	//this method transforms a JSONObject to a AbstractObject;
 	public AbstractObject JSONToObject();
-	public String URLToURLDownload();
+	//this method transforms the JSONObject to a String that represents 
+	//the downloadable link of a File
+	public String JSONObjectToURLDownload();
 }
---
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/converters/ImplementServices.java
@@ -0,0 +1,43 @@
+package univpm.esame.ProgettoOOP.converters;
+
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import univpm.esame.ProgettoOOP.model.AbstractObject;
+
+public class ImplementServices implements AllServices{
+
+	@Override
+	public JSONArray APIToJSONArray() {
+		
+		return null;
+	}
+
+	@Override
+	public JSONObject APIToJSONObject() {
+		
+		return null;
+	}
+
+	@Override
+	public ArrayList<AbstractObject> JSONArrayToObject() {
+		
+		return null;
+	}
+
+	@Override
+	public AbstractObject JSONToObject() {
+		
+		return null;
+	}
+
+	@Override
+	public String JSONObjectToURLDownload() {
+		
+		return null;
+	}
+
+		
+}
