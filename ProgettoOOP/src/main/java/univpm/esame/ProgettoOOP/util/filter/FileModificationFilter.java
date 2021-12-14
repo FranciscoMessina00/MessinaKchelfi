c84c9087e808ea9c3561bcdb2212c9eb8fe72d21
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-15 00:20:55 +0100

    Aggiuntafiltroultimamodifica

---
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/FileModificationFilter.java
@@ -0,0 +1,21 @@
+package univpm.esame.ProgettoOOP.util.filter;
+
+import univpm.esame.ProgettoOOP.model.File;
+import univpm.esame.ProgettoOOP.util.filter_main.Filter;
+
+public class FileModificationFilter extends Filter{
+    private String input;
+
+    public FileModificationFilter (Object input) throws Exception{
+		if (input instanceof String) 
+			this.input = (String) input;
+		else throw new Exception("Error");
+		}
+
+       public boolean doFiltro (File file){
+           if ((input))     //Aggiungere Tag di ultima modifica
+               return true;
+           else return false;       
+        }
+    
+}
