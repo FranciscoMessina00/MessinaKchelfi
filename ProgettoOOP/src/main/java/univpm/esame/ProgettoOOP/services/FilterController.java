2ff51dfb4da74a8d0e7ff2dc10b2fc0cd941e46f
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-16 10:07:50 +0100

    filercontroller

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/services/FilterController.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/services/FilterController.java
@@ -40,59 +40,12 @@ public class FilterController {
 			throw new Exception("The filter value must be an array");
 		String operator="";
 		boolean flag=false;
-		HashMap<String, Object> filterMap = new HashMap<>();
+		
 		
 		if (array.isEmpty()) 
 			throw new Exception("The filter can't be empty");
 			
-		Filter filter = new Filter();
-		Iterator<?> it = array.iterator();
-		while(it.hasNext()) {
-			filterMap = (HashMap<String, Object>)it.next();
-			filterParameters = new ArrayList<>(filterMap.keySet());
-			filterValues = new ArrayList<>(filterMap.values());
-			
-			if (filterParameters.size() == 0 || filterParameters.size() > 2)
-				throw new Exception("There are no parmeters");
-			if(filterParameters.size()>1 && !filterParameters.get(1).equals("oprator"))
-				throw new Exception("To interlock filters use \"operator\"");
-			
-			filter = filterIdentification(filterParameters.get(0), filterValues.get(0));
-			
-			if (!flag) {
-				Iterator<AbstractObject> it2 = rawData.iterator();
-				while (it2.hasNext()) {
-					AbstractObject file = it2.next();
-					if (filter.doFilter(file) && !filteredData.contains(file))
-						filteredData.add(file);
-				}
-			}
-			else {
-				Iterator<AbstractObject> it2 = filteredData.iterator();
-				while (it2.hasNext()) {
-					AbstractObject file = it2.next();
-					if (!filter.doFilter(file))
-						it2.remove();
-				}
-			}
-			
-			if(filterParameters.size()>1) {
-				operator = (String)filterValues.get(1);
-				if(operator.equalsIgnoreCase("and"))
-					flag=true;
-				else if(operator.equalsIgnoreCase("o r"))
-					flag=false;
-				else throw new Exception("The conditional operator must"
-						+ " be \"and\"/\"or\"");
-				if (!it.hasNext()) 
-					throw new Exception ("After a conditional operator there must be another fillter-object ");
-			}
-			
-		}
 		
-		return filteredData;
-	}
-	
 
 	private static Filter filterIdentification(String filterParameter, Object filterValue)
 	throws Exception{
