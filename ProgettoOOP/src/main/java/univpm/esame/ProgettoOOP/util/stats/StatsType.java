b0607b7adb09a17491e0d9899a4d53e6791a32bd
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-18 00:08:56 +0100

    prova

---
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsType.java
@@ -0,0 +1,32 @@
+package univpm.esame.ProgettoOOP.util.stats;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import univpm.esame.ProgettoOOP.exception.IncorrectFormatException;
+import univpm.esame.ProgettoOOP.model.AbstractObject;
+import univpm.esame.ProgettoOOP.model.File;
+
+
+public class StatsType {
+	
+	public static <Path> File getStatsType(ArrayList<AbstractObject> path) {
+
+	AbstractObject file = new File();		
+	HashMap<Path, String> folders = new HashMap<>();
+	Iterator<?> it = path.iterator();
+
+		while(it.hasNext()) {
+			file = (File)it.next();
+			
+			if(file.getTag().equals("Folder")) { 
+			throw new IncorrectFormatException ("I cant do this for a folder");
+			}
+
+		}
+	}	return null;	
+}
+
+	
+
