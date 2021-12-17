d04fbbe93c1231d3778e279c21791af6cecb71bb
FranciscoMessina00 <s1092353@studenti.univpm.it>
2021-12-17 10:49:18 +0100

    Aggiunta elemento a interfaccia filtro

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/Filter.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/filter/Filter.java
@@ -7,4 +7,5 @@ import univpm.esame.ProgettoOOP.model.AbstractObject;
 public interface Filter {
 	public ArrayList<AbstractObject> filterByExtension(ArrayList<AbstractObject> allFiles,String extension);
 	public ArrayList<AbstractObject> filterByName(ArrayList<AbstractObject> allFiles,String name);
+	public ArrayList<AbstractObject> filterBoth(ArrayList<AbstractObject> allFiles,String fullName);
 }
