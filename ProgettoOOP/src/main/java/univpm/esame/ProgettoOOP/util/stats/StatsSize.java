de58dcf42b820d8ca6fa7b5a9580fda053881393
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-21 10:28:28 +0100

    stats

---
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsSize.java
@@ -0,0 +1,15 @@
+package univpm.esame.ProgettoOOP.util.stats;
+
+import java.util.ArrayList;
+
+import univpm.esame.ProgettoOOP.model.AbstractObject;
+
+public class StatsSize {
+    private long param;
+	private String contatore;
+    public StatsSize(long param, String contatore) {
+		this.param=param;
+		this.contatore=contatore;
+    }
+    public ArrayList<AbstractObject> doStatsSize(ArrayList<AbstractObject> files) throws Exception{
+}
