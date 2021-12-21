8e40deac76ad86e80b56b08a3e9787aaed50f0c0
FranciscoMessina00 <s1092353@studenti.univpm.it>
2021-12-21 11:40:18 +0100

    Stats funzia

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsType.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsType.java
@@ -12,7 +12,7 @@ import univpm.esame.ProgettoOOP.model.Folder;
 public class StatsType {
 
 	public static HashMap<String,Integer> getStatsType(ArrayList<AbstractObject> files) throws TypeNotRecognisedException {
-		
+
 		HashMap<String,Integer> hmap = new HashMap<>();
 
 		for(int i=0; i<files.size()-1; i++){
@@ -21,18 +21,18 @@ public class StatsType {
 				if(hmap.containsKey(file.getExtension())) {
 					hmap.replace(file.getExtension(), hmap.get(file.getExtension())+1);
 				}
-					else hmap.put(file.getExtension(), 1);
+				else hmap.put(file.getExtension(), 1);
 
 			}
 			else if (files.get(i) instanceof Folder){
-				if(hmap.containsKey("Folders")) {
-					hmap.replace("Folders", hmap.get("Folders")+1);
-			}	
-				else hmap.put("Folders", 1);
+				if(hmap.containsKey("folders")) {
+					hmap.replace("folders", hmap.get("folders")+1);
+				}	
+				else hmap.put("folders", 1);
 			}
-		else throw new TypeNotRecognisedException("Type not recognised asdf");
+			else throw new TypeNotRecognisedException("Type not recognised");
 		}
-			
+
 		return hmap;
 	}
 }
