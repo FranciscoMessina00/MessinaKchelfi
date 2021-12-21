c5b38b807e2e82792caf37188eda7e3fc8925b10
Amine Kchelfi <aminekchelfi@gmail.com>
2021-12-21 11:35:06 +0100

    ciclostats

--- ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsType.java
+++ ProgettoOOP/src/main/java/univpm/esame/ProgettoOOP/util/stats/StatsType.java
@@ -2,7 +2,6 @@ package univpm.esame.ProgettoOOP.util.stats;
 
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import univpm.esame.ProgettoOOP.exception.TypeNotRecognisedException;
 import univpm.esame.ProgettoOOP.model.AbstractObject;
@@ -15,23 +14,25 @@ public class StatsType {
 	public static HashMap<String,Integer> getStatsType(ArrayList<AbstractObject> files) throws TypeNotRecognisedException {
 		
 		HashMap<String,Integer> hmap = new HashMap<>();
-		Iterator<?> it = files.iterator();
-		while (it.hasNext()){
-			if (((AbstractObject)it.next()).getTag().equals("File")){
-				File file = (File)it.next();
+
+		for(int i=0; i<files.size()-1; i++){
+			if (files.get(i) instanceof File){
+				File file = (File)files.get(i);
 				if(hmap.containsKey(file.getExtension())) {
 					hmap.replace(file.getExtension(), hmap.get(file.getExtension())+1);
-				}else hmap.put(file.getExtension(), 1);
+				}
+					else hmap.put(file.getExtension(), 1);
 
 			}
-			else if (((AbstractObject)it.next()).getTag().equals("Folder")){
+			else if (files.get(i) instanceof Folder){
 				if(hmap.containsKey("Folders")) {
 					hmap.replace("Folders", hmap.get("Folders")+1);
-				}else hmap.put("Folders", 1);
+			}	
+				else hmap.put("Folders", 1);
 			}
-	//		else throw new TypeNotRecognisedException("Type not recognised asdf");
-
+		else throw new TypeNotRecognisedException("Type not recognised asdf");
 		}
+			
 		return hmap;
 	}
 }
